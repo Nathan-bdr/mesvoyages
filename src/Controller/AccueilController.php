1ee1ada596da7aa460846e35e8a211d2e5207027
Nathan Boudier <nathan.boudier@gmail.com>
2025-10-10 09:45:07 +0200

    Phase 21 : page d'accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,15 +2,33 @@
 
 namespace App\Controller;
 
+use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController {
     
+    /**
+     *
+     * @var VisiteRepository
+     */
+    private $repository;
+
+    /**
+     * 
+     * @param VisiteRepository $repository
+     */
+    public function __construct(VisiteRepository $repository) {
+        $this->repository = $repository;
+    }  
+    
     #[Route('/', name: 'accueil')]
     public function index(): Response {
-        return $this->render("pages/accueil.html.twig");
+        $visites = $this->repository->findAllLasted(2);
+        return $this->render("pages/accueil.html.twig", [
+            'visites' => $visites
+        ]);
     }
     
 }
